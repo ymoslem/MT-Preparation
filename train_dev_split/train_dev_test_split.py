75af822f21fc67257b71be8a084669217c3bfb89
Yasmin Moslem <48152713+ymoslem@users.noreply.github.com>
2020-12-28 22:54:09 +0000

    Add train_dev_test_split.py

---
+++ train_dev_split/train_dev_test_split.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Splitting the parallel dataset into train, development and test datasets for Machine Translation
+# Command: python3 train_dev_split.py <dev_segment_number> <test_segment_number> <source_file_path> <target_file_path>
+
+
+import pandas as pd
+import numpy as np
+from sacremoses import MosesTokenizer
+import re
+import csv
+import sys
+
+# display(df) works only if you are in IPython/Jupyter Notebooks or enable:
+# from IPython.display import display
+
+
+segment_no_dev = sys.argv[1]    # Number of segments in the dev set
+segment_no_test = sys.argv[2]    # Number of segments in the test set
+source_file = sys.argv[3]   # Path to the source file
+target_file = sys.argv[4]   # Path to the target file
+
+
+def extract_dev(segment_no_dev, segment_no_test, source_file, target_file):
+
+    df_source = pd.read_csv(source_file, names=['Source'], sep="\n", quoting=csv.QUOTE_NONE, error_bad_lines=False)
+    df_target = pd.read_csv(target_file, names=['Target'], sep="\n", quoting=csv.QUOTE_NONE, error_bad_lines=False)
+    df = pd.concat([df_source, df_target], axis=1)  # Join the two dataframes along columns
+    print("Dataframe shape:", df.shape)
+
+
+    # Delete rows with empty cells (source or target)
+    df = df.dropna()
+
+    print("--- Empty Cells Deleted", "--> Rows:", df.shape[0])
+
+
+    # Extract Dev set from the main dataset
+    df_dev = df.sample(n = int(segment_no_dev))
+    df_train = df.drop(df_dev.index)
+
+    # Extract Test set from the main dataset
+    df_test = df.sample(n = int(segment_no_test))
+    df_train = df.drop(df_test.index)
+
+    # Write the dataframe to two Source and Target files
+    source_file_train = source_file+'.train'
+    target_file_train = target_file+'.train'
+
+    source_file_dev = source_file+'.dev'
+    target_file_dev = target_file+'.dev'
+
+    source_file_test = source_file+'.test'
+    target_file_test = target_file+'.test'
+
+    df_dic_train = df_train.to_dict(orient='list')
+
+
+    with open(source_file_train, "w") as sf:
+        sf.write("\n".join(line for line in df_dic_train['Source']))
+
+    with open(target_file_train, "w") as tf:
+        tf.write("\n".join(line for line in df_dic_train['Target']))
+
+
+    df_dic_dev = df_dev.to_dict(orient='list')
+
+    with open(source_file_dev, "w") as sf:
+        sf.write("\n".join(line for line in df_dic_dev['Source']))
+
+    with open(target_file_dev, "w") as tf:
+        tf.write("\n".join(line for line in df_dic_dev['Target']))
+
+
+    df_dic_test = df_test.to_dict(orient='list')
+
+    with open(source_file_test, "w") as sf:
+        sf.write("\n".join(line for line in df_dic_test['Source']))
+
+    with open(target_file_test, "w") as tf:
+        tf.write("\n".join(line for line in df_dic_test['Target']))
+
+
+    print("--- Wrote Files")
+    print("Done!")
+    print("Output files", *[source_file_train, target_file_train, source_file_dev, target_file_dev, source_file_test, target_file_test], sep="\n")
+
+
+
+extract_dev(segment_no_dev, segment_no_test, source_file, target_file)
